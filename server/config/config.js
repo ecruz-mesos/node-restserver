91866e78f4183e7f48d5e3b5713ac65719d863cc
ecruz@mesos.cl <ecruz@mesos.cl>
2021-01-10 17:40:33 -0300

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,10 @@ process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-// urlDB = 'mongodb://localhost:27017/cafe'
-// } else {
-urlDB = 'mongodb+srv://strider:0ZSTXQjjtdacGevs@clusterecr.jp80o.mongodb.net/test'
-    // }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = process.env.MONGO_URI
+}
 
 process.env.URLDB = urlDB
